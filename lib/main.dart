ae7622419cdfc19e7db5095530167006a6f53ce0
Najifa <najifachy2003@gmail.com>
2025-11-14 16:50:40 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:assignment7_burhan_1178/home_page.dart';
+import 'package:assignment7_Najifa_136/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Assignment 7 - Najifa 136",
